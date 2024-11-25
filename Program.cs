6543b87aa5d8a3bf96f220d6ae4a8f89091c75b8
Oliver Mineur <olivermineur@gmail.com>
2024-11-25 12:04:15 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _100_People_Uppgift
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            
+        }
+    }
+}
