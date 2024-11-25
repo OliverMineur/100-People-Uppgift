8fc020f3b04a9ced10b11680b346b2d932cccb33
Oliver Mineur <olivermineur@gmail.com>
2024-11-25 12:11:56 +0100

    made props and list

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _100_People_Uppgift
+{
+    internal class People
+    {
+        private string FirstName { get; }
+        private string LastName { get; }
+        private DateTime Birthyear { get; }
+    }
+}
